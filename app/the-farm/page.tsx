7fe573d814b4a6658258e136bca4d4f0c65ddef7
socialscript <info@socialscript.in>
2024-12-02 06:48:55 +0000

    build fix and new default page added

--- app/the-farm/page.tsx
+++ app/the-farm/page.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 
-const page = () => {
+const TheFarmPage = () => {
   return (
     <div>
       <h1>The Farm</h1>
     </div>
   )
 }
-
-export default page
+export default TheFarmPage
